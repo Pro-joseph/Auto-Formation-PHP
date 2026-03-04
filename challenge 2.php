dfaa9078c75ed76e6dfaa32510d6aac6799b408d
Pro-joseph <jdirayoussef1997@gmail.com>
2026-03-04 15:07:03 +0000

    Ticketing

--- challenge 2.php
+++ challenge 2.php
@@ -7,12 +7,12 @@
 </head>
 <body>
     <?php
-    $age = 90;
+    $age = 100;
 
     if($age <= 12 ){
         echo "the price is 20 DH"."<br>";
         echo "Special: Children's Menu included!";
-    }elseif($age = range(12, 18)){
+    }elseif($age > 12 && $age <= 18){
         echo "the price is 40 DH";
     }elseif($age >= 60){
         echo "the price is 30 DH (Senior discount)";
