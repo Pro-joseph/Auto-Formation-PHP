2bfe5018f03ebd66965dadc877783e887a53ed35
Pro-joseph <jdirayoussef1997@gmail.com>
2026-03-04 14:09:03 +0000

    Teabill

--- challenge 1.php
+++ challenge 1.php
@@ -8,28 +8,29 @@
 <body>
     <?php
     $TeaPrix = 10;
-    $Teatotal = 7;
+    $Teatotal = 20;
     $Discount = 0.20;
     $student = true;
-
+//oprations
     $QntTea = $TeaPrix * $Teatotal;
     $Discou = $QntTea * $Discount;
 
-
+//student
     if($student = true){
         
         $Pris = $QntTea - $Discou;
-        
     }
+//Every 5 teas
+    $More_five = $Teatotal / 5;
     if($Teatotal >= 5){
-        $Total = $Pris - 1 ;
+        $Total = $Pris - intval($More_five) ;
     }
     
 
     echo "Tea ordered: $QntTea DH"."<br>";
     echo "Discount 20%: $Discou DH"."<br>";
     echo "Tea Bill: $Pris DH"."<br>";
-    echo "Discount For more than 5 orders: 1 DH:"."<br>";
+    echo "Discount For more than 5 orders:".intval($More_five) ." DH:"."<br>";
     echo "Tea Bill Total is: $Total DH"."<br>";
 
     ?>
