3444c91d59a780c3ed53b2d1eb830b4031c6303d
Pro-joseph <jdirayoussef1997@gmail.com>
2026-03-05 03:43:01 +0000

    Tea Bill

---
+++ challenge1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $Tea_Prix = 10;
+    $Tea_ordered = 7;
+    $Discount = 0.20;
+    $client = array("Particulier", "Student");
+//oprations
+    $Tea_Total = $Tea_Prix * $Tea_ordered;
+    $Discount_Student = $Tea_Total * $Discount;
+
+//student
+    if($client == $client[1]){
+        
+        $Prix_Student = $Tea_Total - $Discount_Student;
+        
+    }
+//particuler
+        $Prix_parti = $Tea_Total;
+
+//Every 5 teas for student
+    $More_five = $Tea_ordered / 5;
+    
+    if($Tea_ordered >= 5){
+        $Total_Student = $Prix_Student - intval($More_five);
+    }
+// for particuler
+        $Total_parti = $Prix_parti - intval($More_five);
+
+    
+    echo "Tea ordered: $Tea_Total DH"."<br>";
+    echo "Discount 20%: $Discount_Student DH"."<br>";
+    echo "Tea Bill: $Prix_Student DH"."<br>";
+    echo "Discount For more than 5 orders:".intval($More_five) ." DH:"."<br>";
+    echo "Tea Bill Total For student is: $Total_Student DH"."<br>";
+    echo "Tea Bill Total For Particuler is: $Total_parti DH"."<br>";
+
+
+    ?>
+</body>
+</html>
