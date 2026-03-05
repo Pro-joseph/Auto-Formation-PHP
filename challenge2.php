d0fca5fde7dc0d5046e10db334d16d281d354689
Pro-joseph <jdirayoussef1997@gmail.com>
2026-03-05 04:04:28 +0000

    Teackiting system

---
+++ challenge2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $age = 10;
+
+    if($age <= 12 ){
+        echo "the price is 20 DH"."<br>";
+        echo "Special: Children's Menu included!";
+    }elseif($age > 12 && $age <= 18){
+        echo "the price is 40 DH";
+    }elseif($age >= 60){
+        echo "the price is 30 DH (Senior discount)";
+    }else{
+        echo "the price is 60 DH";
+    }
+
+
+    ?>
+</body>
+</html>
